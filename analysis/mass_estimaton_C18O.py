3233805aea8eb802d6dc0108cb32f70a596c0782
Teresa Valdivia <tere.v.1521@gmail.com>
2021-06-07 11:35:46 +0200

    Applied primary beam corrected uncertainties

--- analysis/mass_estimaton_C18O.py
+++ analysis/mass_estimaton_C18O.py
@@ -12,6 +12,7 @@ import regions
 import matplotlib.pyplot as plt
 from astropy.constants import G
 from astropy.coordinates import SkyCoord, FK5
+from astropy.modeling.models import Gaussian2D
 import aplpy
 import copy
 import velocity_tools.stream_lines as SL
@@ -125,12 +126,15 @@ filenameC18O = '../' + C18O_2_1 + '_pbcor_reprojectH2COs_mom0_l'
 filenameC18Okink = '../' + C18O_2_1 + '_pbcor_reprojectH2COs_mom0_l_kink'
 tablefile = 'M_H2_Tex_fixed_mom0_pbcor_unc.csv'
 tablefilekink = 'M_H2_Tex_fixed_mom0_pbcor_kink_unc.csv'
+tablefilemacc = 'M_Mdot_Tex_fixed_mom0_pbcor_binned_unc.csv'
 rms = 2.4041 * u.K * u.km/u.s
 NC18Ofilename = 'N_C18O_constantTex_{0}K_mom0_pbcor.fits'
 uNC18Ofilename = 'N_C18O_unc_constantTex_{0}K_mom0_pbcor.fits'
 NC18Ofilenamekink = 'N_C18O_constantTex_{0}K_mom0_pbcor_kink.fits'
 uNC18Ofilenamekink = 'N_C18O_unc_constantTex_{0}K_mom0_pbcor_kink.fits'
 NC18Oplotname = 'N_C18O_constantTex_{0}K_mom0_pbcor.pdf'
+radius3Dmapname = 'column_dens_maps/distance_3D_map.fits'
+radiusmapname = 'column_dens_maps/distance_2D_map.fits'
 X_C18O = 5.9e6 # Look for Frerking et al 1982
 # this is the X_C18O value used in Nishimura et al 2015 for Orion clouds
 distance = (dist_Per50 * u.pc).to(u.cm)
@@ -139,80 +143,35 @@ velinit = 5.5 * u.km/u.s
 velend = 9.5 * u.km/u.s
 Texlist = np.array([10,11,12,13,14,15])* u.K
 B0 = (54891.420 * u.MHz).to(1/u.s)
-
+binsize = 360 # au
 
 '''
 End inputs
 '''
 
-# reproject the C18O to the H2CO wcs
-# if not os.path.exists(filenameC18O+'_reprojectH2COs_2.fits'):
-#     cubeH2CO = SpectralCube.read(filenameH2CO+'.fits').with_spectral_unit(u.km/u.s).spectral_slab(velinit,velend)
-#     cubeC18O = SpectralCube.read(filenameC18O+'.fits').with_spectral_unit(u.km/u.s)
-#     spectral_grid_objective = cubeH2CO.spectral_axis
-#     # spectral reprojection
-#     cubeC18O = cubeC18O.spectral_interpolate(spectral_grid_objective)
-#     # spatial reprojection
-#     cubeC18O = cubeC18O.reproject(cubeH2CO.header) #beam is still there
-#     cubeC18O.write(filenameC18O+'_reprojectH2COs_2.fits')
-# else:
-#     cubeC18O = SpectralCube.read(filenameC18O+'_reprojectH2COs_2.fits').with_spectral_unit(u.km/u.s)
-#
-# # Now leave out all that is not streamer
-# if not os.path.exists(filenameC18O+'_H2COmasked_'+str(snratio)+'sigma_2.fits'):
-#     if not 'cubeH2CO' in globals():
-#         #  Just load if not loaded before
-#         cubeH2CO = SpectralCube.read(filenameH2CO+'.fits').with_spectral_unit(u.km/u.s).spectral_slab(velinit,velend)
-#     # mask the cube where there is emission
-#     masked_cube = cubeC18O.with_mask(cubeH2CO > snratio* rms)
-#     region_streamer = '../data/region_streamer_l.reg'
-#     regio = regions.read_ds9(region_streamer)
-#     streamer_cube = masked_cube.subcube_from_regions(regio)
-#     streamer_cube.write(filenameC18O+'_H2COmasked_'+str(snratio)+'sigma_2.fits')
-#
-# streamer_cube = SpectralCube.read(filenameC18O+'_H2COmasked_'+str(snratio)+'sigma_2.fits')
-# # We call it new because it is the header of the masked and cut C18O
-# # newheaderC18O = cubeC18O.header
-# newheaderC18O = streamer_cube.header
-# deltara = (newheaderC18O['CDELT1'] * u.deg).to(u.rad).value
-# deltadec = (newheaderC18O['CDELT2'] * u.deg).to(u.rad).value
-#
-# # Change cube to K
-# k_streamer_cube = streamer_cube.to(u.K)
-# # k_streamer_cube.write(filenameC18O+'_testK.fits')
-#
-# # do a moment 0
-# # As it is the moment 0 of C18O where H2CO is positive,C18O can be negative
-# mom0 = k_streamer_cube.moment(order=0)
-# NC18Oheader = mom0.header
-# mom0 = mom0.value * mom0.unit #Transform from type Projection to type Quantity
-# mom0[np.where(mom0.value < 0.0)] = np.nan * u.K * u.km/u.s
-# wcsmom = WCS(newheaderC18O).celestial
-# # for now,lets assume a constant Tex
-#
-# NC18Oheader['bmaj'] = newheaderC18O['bmaj']
-# NC18Oheader['bmin'] = newheaderC18O['bmin']
-# NC18Oheader['bpa'] = newheaderC18O['bpa']
-#
-# #save the moment 0
-# if not os.path.exists(filenameC18O+'_H2COmasked_'+str(snratio)+'sigma_mom0_2.fits'):
-#     NC18Omomheader = NC18Oheader.copy()
-#     NC18Omomheader['bunit'] = 'K km s-1'
-#     newmom0hdu = fits.PrimaryHDU(data=mom0.value, header=NC18Omomheader)
-#     newmom0hdu.writeto(filenameC18O+'_H2COmasked_'+str(snratio)+'sigma_mom0_2.fits')
 NC18Oheader = fits.getheader(filenameC18O+'.fits')
+restfreq = NC18Oheader['restfreq'] * u.Hz
+primbeamFWHM = pb_noema(restfreq).to(u.deg).value
 deltara = (NC18Oheader['CDELT1'] * u.deg).to(u.rad).value
 deltadec = (NC18Oheader['CDELT2'] * u.deg).to(u.rad).value
 wcsmom = WCS(NC18Oheader)
+ra0_pix, dec0_pix = wcsmom.celestial.all_world2pix(ra_Per50.value, dec_Per50.value, 0)
+# we need a map of the primary beam response to calculate the unc. properly
 mom0 = fits.getdata(filenameC18O+'.fits') *u.K * u.km/u.s
-u_mom0 = rms * np.ones(np.shape(mom0))
+leny, lenx = np.shape(mom0)
+yy, xx = np.mgrid[0:leny, 0:lenx]
+beamresponse = Gaussian2D(amplitude=1, x_mean=ra0_pix, y_mean=dec0_pix, x_stddev=primbeamFWHM/2.35/(deltadec*u.rad).to(u.deg).value,y_stddev=primbeamFWHM/2.35/(deltadec*u.rad).to(u.deg).value)(xx,yy)
+# u_mom0 = rms * np.ones(np.shape(mom0))
+u_mom0 = rms / beamresponse
 mom0 = unumpy.uarray(mom0.value,u_mom0.value) # they cannot be with units
 mom0kink = fits.getdata(filenameC18Okink+'.fits') *u.K * u.km/u.s
-u_mom0kink = rms * np.ones(np.shape(mom0kink))
+# u_mom0kink = rms * np.ones(np.shape(mom0kink))
+u_mom0kink = rms / beamresponse
 mom0kink = unumpy.uarray(mom0kink.value,u_mom0kink.value)
 
 NC18Oheader['bunit'] = 'cm-2'
 
+# Here we calculate the NC18O map and statistics
 if os.path.exists(tablefilekink) and os.path.exists(tablefile):
     results_mass = pd.read_csv(tablefile)
     results_mass_kink = pd.read_csv(tablefilekink)
@@ -330,7 +289,7 @@ wcsmap = WCS(fits.getheader('column_dens_maps/'+NC18Ofilename.format(10.0))).cel
 
 NH2map = NC18Omap * X_C18O # * (u.cm**-2)
 NH2mapkink = NC18Omapkink * X_C18O # * (u.cm**-2)
-leny, lenx = np.shape(NH2map)
+
 
 # This is to have an idea of the total mass
 M_s = 1.71*u.Msun
@@ -343,15 +302,13 @@ leny, lenx = np.shape(NC18Omap)
 M_acc = unumpy.uarray(results_mass_kink['M (M_sun)'].values, results_mass_kink['u M (M_sun)'].values)
 M_dot = [M_acc / t_ff[0].value, M_acc / t_ff[1].value]
 
-
 # Now, we separate the streamer in bins
 # Now we do the same for the streamer-calculated distances
 
-xx, yy = np.meshgrid(range(lenx), range(leny))
-
 distance_map = distance_physical(ra_Per50.value,dec_Per50.value, xx, yy, NC18Oheader)
-binsize = 200 # au
 
+
+# We load the model
 modelname = 'H2CO_0.39Msun_env'
 fileinpickle = 'streamer_model_'+modelname+'_params'
 pickle_in = open(fileinpickle+'.pickle', "rb")
@@ -369,12 +326,13 @@ PA_ang = streamdict['PA']
 rc = SL.r_cent(mass=Mstar[1], omega=omega0, r0=r0)
 
 # y1 is in au
+mass_streamer_table = pd.DataFrame()
 dist_streamer = np.sqrt(x1**2+y1**2+z1**2)
 dist_projected = np.sqrt(x1**2+z1**2)
 
-
-radiuses = np.arange(0,3300, binsize) # list of streamer lengths we want to sample
-binradii = np.arange(100,3300,binsize) # u.AU  of  the streamer length
+radiuses = np.arange(0.,3200., binsize) # list of projected lengths we want to sample
+# binradii = np.arange(binsize/2,3300.,binsize) # u.AU  of the streamer length
+binradii = np.zeros(len(radiuses)-1) * u.AU
 masses = unumpy.uarray(np.zeros(len(binradii)), np.zeros(len(binradii)))
 masseskink = unumpy.uarray(np.zeros(len(binradii)), np.zeros(len(binradii)))
 # masses = np.zeros(len(binradii)) * u.Msun
@@ -382,92 +340,59 @@ masseskink = unumpy.uarray(np.zeros(len(binradii)), np.zeros(len(binradii)))
 times = np.zeros((len(binradii),2)) * u.yr
 # m_acclist = np.zeros((len(binradii),2)) * u.Msun / u.yr
 # m_acclistkink = np.zeros((len(binradii),2)) * u.Msun / u.yr
-m_acclist = unumpy.uarray(np.zeros((len(binradii),2)), np.zeros((len(binradii),2)))
-m_acclistkink = unumpy.uarray(np.zeros((len(binradii),2)), np.zeros((len(binradii),2)))
+m_acclist = unumpy.uarray(np.zeros(len(binradii)), np.zeros(len(binradii)))
+m_acclistkink = unumpy.uarray(np.zeros(len(binradii)), np.zeros(len(binradii)))
+radius3Dmap = np.zeros(np.shape(mom0)) * np.nan
+# We need to save the previous limit
+# aux_min = 0.
+
+mass_streamer_table['2D bin minimum (au)'] = radiuses[:len(radiuses)-1]
+mass_streamer_table['2D bin maximum (au)'] = radiuses[1:]
 
 for i in range(len(radiuses)-1):
-    xzrange = dist_projected[np.where((dist_streamer.value>radiuses[i]) & (dist_streamer.value<radiuses[i+1]))]
-    mask = np.where((distance_map>np.amin(xzrange.value)) & (distance_map<np.amax(xzrange.value)))
-    # mask = np.where((distance_map>radiuses[i]) & (distance_map<radiuses[i+1]))
+    # we want to make a map of the bins, so we need to save the indexes
+    mask = np.where((distance_map>radiuses[i]) & (distance_map<radiuses[i+1]))
+    # This is the mask of the pixels in the map
+    # within these pixels are the x,z of the streamer
+    # streamer distances within these bin of projected distances
+    streamer_distances = dist_streamer[np.where((dist_projected.value>radiuses[i]) & (dist_projected.value<radiuses[i+1]))]
+    # Now, what is the mean 3D distance for this bin
+    binradii[i] = np.mean(streamer_distances)
+    # xzrange = dist_projected[np.where((dist_streamer.value>radiuses[i]) & (dist_streamer.value<radiuses[i+1]))]
+    # mask = np.where((distance_map>np.amin(xzrange.value)) & (distance_map<np.amax(xzrange.value)))
+    # aux_max = np.amax(xzrange.value)
+    # mask = np.where((distance_map>aux_min) & (distance_map<=aux_max))
+    # radius3Dmap[mask] = binradii[i]
+    # aux_min = aux_max
     NH2tot = np.nansum(NH2map[mask])
     NH2totkink = np.nansum(NH2mapkink[mask])
     masses[i] = M_hydrogen2(NH2tot, mu_H2, distance, deltara, deltadec)
     masseskink[i] = M_hydrogen2(NH2totkink, mu_H2, distance, deltara, deltadec)
-    times[i] = t_freefall(binradii[i]*u.AU, Mstar)
-    m_acclist[i] = [masses[i] / times[i,0].value, masses[i] / times[i,1].value]
-    m_acclistkink[i] = [masseskink[i] / times[i,0].value, masseskink[i] / times[i,1].value]
+    times[i] = t_freefall(binradii[i], Mstar)
+    m_acclist[i] = masses[i] / times[i,1].value
+    m_acclistkink[i] = masseskink[i] / times[i,1].value
+# indexnans = np.where(np.isnan(unumpy.nominal_values(mom0kink)))
+# radius3Dmap[indexnans] = np.nan
+# if not os.path.exists(radius3Dmapname):
+#     radius3Dheader = NC18Oheader.copy()
+#     radius3Dheader['bunit'] = 'au'
+#     hdu = fits.PrimaryHDU(data=radius3Dmap, header=radius3Dheader)
+#     hdu.writeto(radius3Dmapname)
 
-fig = plt.figure(figsize=(6,6))
-ax3 = fig.add_subplot(313)
-ax3.errorbar(binradii[1:], unumpy.nominal_values(m_acclist[1:,1]), yerr=unumpy.std_devs(m_acclist[1:,1]), marker='o', linestyle='none', mfc='none', mec='r', ecolor='r')
-ax3.errorbar(binradii[1:], unumpy.nominal_values(m_acclistkink[1:,1]), yerr=unumpy.std_devs(m_acclistkink[1:,1]), marker='o', linestyle='none', mfc='r', mec='r', ecolor='r')
-# ax3.scatter(binradii[1:], m_acclistkink[1:,1].value, s=50, facecolors='r', edgecolors='r')
-ax3.set_yscale('log')
-ax3.set_xlabel('Distance from Protostar (au)')
-ax3.set_ylabel(r'$\dot{M}$ (M$_{\odot}$ yr$^{-1}$)')
-ax3.annotate('bin size = {} au'.format(binsize), (0.6,0.8), xycoords='axes fraction', size=14)
-ax3.axvline(293, color='k', linestyle='dashed')
-ax3.axvline(rc.value, color='b', linestyle='dotted')
-ax3.set_xlim([0,3300])
+mass_streamer_table['3D distance (au)'] = binradii
+mass_streamer_table['Mass wo kink (Msun)'] = unumpy.nominal_values(masses)
+mass_streamer_table['u Mass wo kink (Msun)'] = unumpy.std_devs(masses)
+mass_streamer_table['Mass w kink (Msun)'] = unumpy.nominal_values(masseskink)
+mass_streamer_table['u Mass w kink (Msun)'] = unumpy.std_devs(masseskink)
+mass_streamer_table['t_ff (yr, M_env = 0.39 Msun)'] = times[:,1]
+mass_streamer_table['Mdot wo kink (Msun yr-1)'] = unumpy.nominal_values(m_acclist)
+mass_streamer_table['u Mdot wo kink (Msun yr-1)'] = unumpy.std_devs(m_acclist)
+mass_streamer_table['Mdot w kink (Msun yr-1)'] = unumpy.nominal_values(m_acclistkink)
+mass_streamer_table['u Mdot w kink (Msun yr-1)'] = unumpy.std_devs(m_acclistkink)
+mass_streamer_table.to_csv(tablefilemacc)
 
-ax = fig.add_subplot(311, sharex=ax3)
-ax.errorbar(binradii[1:], unumpy.nominal_values(masses[1:])*1e3, yerr=unumpy.std_devs(masses[1:])*1e3, marker='o', linestyle='none', mfc='none', mec='r', ecolor='r', label='No kink')
-ax.errorbar(binradii[1:], unumpy.nominal_values(masseskink[1:])*1e3, yerr=unumpy.std_devs(masseskink[1:])*1e3, marker='o', linestyle='none', mfc='r', mec='r', ecolor='r', label='kink')
-# ax.scatter(binradii[1:], masses[1:].value*1e3, s=50, facecolors='none', edgecolors='r', label='No kink')
-# ax.scatter(binradii[1:], masseskink[1:].value*1e3, s=50, facecolors='r', edgecolors='r',label='kink')
-ax.set_ylabel(r'Mass in bin ($\times 10^3$ M$_{\odot}$)')
-ax.legend(loc=4)
-ax.axvline(293, color='k', linestyle='dashed')
-# ax.annotate('Beam FWHM',(0.12,0.8), xycoords='axes fraction', size=14, rotation=90)
-ax.text(350,0.2, 'Beam FWHM', size=10, rotation=90)
-ax.axvline(rc.value, color='b', linestyle='dotted')
-ax.text(150,0.2, r'$r_c=$'+str(round(rc.value,1))+' au', size=10, rotation=90, color='b')
-
-ax2 = fig.add_subplot(312, sharex=ax3)
-ax2.scatter(binradii[1:], times[1:,1].value, s=50, facecolors='b', edgecolors='b', label=r'$M_{*}=$'+str(Mstar[1]))
-ax2.set_ylabel(r'Free-fall timescale (yr$^{-1}$)')
-ax2.set_yscale('log')
-ax2.legend()
-ax2.axvline(293, color='k', linestyle='dashed')
-ax2.axvline(rc.value, color='b', linestyle='dotted')
-plt.setp(ax2.get_xticklabels(), visible=False)
-plt.setp(ax.get_xticklabels(), visible=False)
-
-# fig.savefig('column_dens_maps/plot_mass_accretion_radius.pdf', dpi=300, bbox_inches='tight')
-
-def fmt(x):
-    s = f"{x:.1f}"
-    if s.endswith("0"):
-        s = f"{x:.0f}"
-    return rf"{s}" if plt.rcParams["text.usetex"] else f"{s} %"
-
-# fig2 = plt.figure(figsize=(4,4))
-# ax = fig2.add_subplot(111, projection=wcsmap)
-# cmap = copy.copy(plt.cm.viridis)
-# cmap.set_bad(np.array((1,1,1))*0.85)
-
-fig3 = plt.figure(figsize=(4,4))
-gc = aplpy.FITSFigure('column_dens_maps/'+NC18Ofilenamekink.format(10.0), figure=fig3)
-gc.show_colorscale()
-gc.add_colorbar()
-gc.add_beam()
-gc.beam.set_color('k')
-gc.beam.set_frame(False)
-gc.set_nan_color(np.array((1,1,1))*0.85)
-gc.colorbar.set_axis_label_text(r'N(C$^{18}$O) (cm$^{-2}$)')
-# fig3.savefig('column_dens_maps/N_C18O_constantTex_{0}K_mom0_pbcor_kink_aplpy.pdf'.format(10.0), dpi=300, bbox_inches='tight')
-
-fig4 = plt.figure(figsize=(4,4))
-ax4 = fig4.add_subplot(111)
-ax4.plot(dist_projected.value,dist_streamer.value-dist_projected.value,'g-')
-ax4.axvline(rc.value, color='b', linestyle='dotted', label=r'$r_c=$'+str(round(rc.value,1))+' au')
-ax4.axvline(293, color='k', linestyle='dashed', label='Beam FWHM')
-ax4.set_xlabel('Projected distance (au)')
-ax4.set_ylabel(r'Difference b/w 3D and projected distance (au)')
-ax4.legend()
-# fig4.savefig(fileinpickle+'_distance_projection.pdf', dpi=300, bbox_inches='tight')
-
-# If we want to know the angle
-
-# angles = (np.arccos(dist_projected.value/dist_streamer.value) * u.rad).to(u.deg).value
-# plt.plot(dist_projected.value,y1.value)
+if not os.path.exists(radiusmapname):
+    radiusheader = NC18Oheader.copy()
+    radiusheader['bunit'] = 'au'
+    hdu = fits.PrimaryHDU(data=distance_map, header=radiusheader)
+    hdu.writeto(radiusmapname)

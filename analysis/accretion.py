Corrected small bug in mass calculation

analysis/accretion.py
@@ -15,6 +15,8 @@ import astropy.units as u
 import matplotlib.pyplot as plt
 
 '''
+This code is set to estimate the mass and accretion rate along the streamer.
+
 Important functions
 '''
 
@@ -60,20 +62,21 @@ def M_hydrogen2_unumpy(N, mu, D, deltara, deltadec):
 '''
 Inputs
 '''
-# Tex_u = ufloat(15, 5)
-Tex_u = 15
-# formatname = str(int(Tex_u.n)) + 'pm' + str(int(Tex_u.s))
-formatname = str(int(Tex_u))
-tablefilemacc = 'M_Mdot_Tex_{0}_mom0_pbcor_rolledbin_unc_tmodel_nodist.csv'.format(formatname)
-modelname = 'H2CO_0.39Msun_env'
+Tex_u = ufloat(15, 5)
+Menv = 0.39 # M_sun
+# Tex_u = 15
+formatname = str(int(Tex_u.n)) + 'pm' + str(int(Tex_u.s))
+# formatname = str(int(Tex_u))
+tablefilemacc = 'M_Mdot_Tex_{0}_mom0_pbcor_rolledbin_unc_tmodel_Menv_{1}Msun.csv'.format(formatname, Menv)
+modelname = 'H2CO_{0}Msun_env'.format(Menv)
 fileinpickle = 'streamer_model_'+modelname+'_params'
 NC18Ofilename = 'N_C18O_constantTex_{0}K_mom0_pbcor.fits'
 uNC18Ofilename = 'N_C18O_unc_constantTex_{0}K_mom0_pbcor.fits'
 NC18Ofilenamekink = 'N_C18O_constantTex_{0}K_mom0_pbcor_kink.fits'
 uNC18Ofilenamekink = 'N_C18O_unc_constantTex_{0}K_mom0_pbcor_kink.fits'
 NC18Oplotname = 'N_C18O_constantTex_{0}K_mom0_pbcor.pdf'
-radius3Dmapname = 'column_dens_maps/distance_3D_map.fits'
-radiusmapname = 'column_dens_maps/distance_2D_map.fits'
+# radius3Dmapname = 'column_dens_maps/distance_3D_map.fits'
+# radiusmapname = 'column_dens_maps/distance_2D_map.fits'
 X_C18O = 5.9e6  # Frerking et al 1982
 # this is the X_C18O value used in Nishimura et al 2015 for Orion clouds
 distance = (dist_Per50 * u.pc).to(u.cm).value
@@ -85,7 +88,7 @@ deltar = 10*u.au  # sample size for the streamline model
 # Central envelope parameters
 M_s = 1.71 * u.Msun
 # M_s = ufloat(1.71, 0.19)
-M_env = 0.39 * u.Msun
+M_env = Menv * u.Msun
 # M_env = ufloat(0.285, 0.105)
 M_disk = 0.58 * u.Msun
 Mstar = (M_s + M_env + M_disk)
@@ -130,7 +133,7 @@ PA_ang = streamdict['PA']
 # We run the streamline model
 (x1, y1, z1), (vx1, vy1, vz1) = SL.xyz_stream(
     mass=Mstar, r0=r0, theta0=theta0, phi0=phi0,
-    omega=omega0, v_r0=v_r0, inc=inc, pa=PA_ang, rmin=10*u.au, deltar=deltar)
+    omega=omega0, v_r0=v_r0, inc=inc, pa=PA_ang, rmin=10*u.au) #, deltar=deltar)
 rc = SL.r_cent(mass=Mstar, omega=omega0, r0=r0)
 
 # we need the pixel location of the streamer for the future
@@ -167,6 +170,7 @@ deltat = (deltas / vel_mean).to(u.yr)
 # we reverse two times to sum from protostar to start point and return the
 # array to its original order
 time_integral_path = np.flip(np.cumsum(np.flip(deltat))).to(u.yr)
+print('The total infall time is '+str(time_integral_path[0]))
 
 # Now, we separate the streamer in bins
 # for the streamer-calculated distances
@@ -205,7 +209,7 @@ for i in range(len(radiuses)):
         dist_projected_mean.value < radiuses2[i]))
     streamer_distances = dist_mean[indexbin]
     streamer_times = time_integral_path[indexbin]
-    streamer_times_theory = time_theory[indexbin]
+    # streamer_times_theory = time_theory[indexbin]
     deltat_bin = deltat[indexbin]
     xs = xstream[indexbin]
     zs = zstream[indexbin]
@@ -249,10 +253,10 @@ mass_streamer_table['u Mass wo kink (Msun)'] = unumpy.std_devs(masses)
 mass_streamer_table['Mass w kink (Msun)'] = unumpy.nominal_values(masseskink)
 mass_streamer_table['u Mass w kink (Msun)'] = unumpy.std_devs(masseskink)
 mass_streamer_table['t_ff streamline (yr, M_env = 0.39 Msun)'] = unumpy.nominal_values(times)
-mass_streamer_table['u t_ff streamline (yr, M_env = 0.39 Msun)'] = unumpy.std_devs(times)
-mass_streamer_table['t_ff streamline kink (yr, M_env = 0.39 Msun)'] = unumpy.nominal_values(timeskink)
-mass_streamer_table['u t_ff streamline kink (yr, M_env = 0.39 Msun)'] = unumpy.std_devs(timeskink)
-mass_streamer_table['deltat_ff streamline (yr, M_env = 0.39 Msun)'] = deltatlist.value
+mass_streamer_table['u t_ff streamline (yr, M_env = {0} Msun)'.format(Menv)] = unumpy.std_devs(times)
+mass_streamer_table['t_ff streamline kink (yr, M_env = {0} Msun)'.format(Menv)] = unumpy.nominal_values(timeskink)
+mass_streamer_table['u t_ff streamline kink (yr, M_env = {0} Msun)'.format(Menv)] = unumpy.std_devs(timeskink)
+mass_streamer_table['deltat_ff streamline (yr, M_env = {0} Msun)'.format(Menv)] = deltatlist.value
 mass_streamer_table['Mdot_in wo kink (Msun yr-1)'] = unumpy.nominal_values(m_inlist)
 mass_streamer_table['u Mdot_in wo kink (Msun yr-1)'] = unumpy.std_devs(m_inlist)
 mass_streamer_table['Mdot_in w kink (Msun yr-1)'] = unumpy.nominal_values(m_inlistkink)

analysis/mass_estimaton_C18O.py
@@ -20,42 +20,8 @@ import astropy.units as u
 This code creates the N(C18O) maps and takes important statistics and quick
 calculations.
 
-Important functions
+Important functions moved to NOEMAsetup
 '''
-
-# M_hydrogen2 moved to NOEMAsetup
-
-
-def J_nu(nu, T):
-    """
-    Calculates the Rayleigh-Jeans equivalent temperature J_nu, in particular to
-    aid in the calculation of the column density of C18O (but is used for any
-    molecule)
-
-    Note that the input parameter nu must have its corresponding unit
-
-    Returns the equivalent temperature in K but no quantity
-    changed the np.exp to umath.exp
-    """
-    over = (h * nu / k_B).to(u.K)
-    under = umath.exp(over.value/T) - 1.
-    return (over.value/under)
-
-
-def Qrot(B0, Tex):
-    """
-    Calculates the partition function of a rigid rotor, diatomic molecule, with
-    the 1st order Taylor approximation
-
-    The partition function is adimensional,  so the function returns a float
-
-    Tex must not be a quantity if it has an associated error
-    """
-    preamble = (k_B / (h * B0)).to(1/u.K)
-    taylorapp = preamble.value * Tex + 1./3.
-    return taylorapp
-
-# Moved NC18O to NOEMAsetup
 
 '''
 Inputs
@@ -63,8 +29,6 @@ Inputs
 # filenames
 filenameC18O = '../' + C18O_2_1 + '_pbcor_reprojectH2COs_mom0_l'
 filenameC18Okink = '../' + C18O_2_1 + '_pbcor_reprojectH2COs_mom0_l_kink'
-# tablefile = 'M_H2_Tex_fixed_mom0_pbcor_unc.csv'
-# tablefilekink = 'M_H2_Tex_fixed_mom0_pbcor_kink_unc.csv'
 tablefile_unumpy = 'M_H2_Tex_{0}_mom0_pbcor_unc.csv'
 NC18Ofilename = 'N_C18O_constantTex_{0}K_mom0_pbcor.fits'
 uNC18Ofilename = 'N_C18O_unc_constantTex_{0}K_mom0_pbcor.fits'
@@ -73,16 +37,13 @@ uNC18Ofilenamekink = 'N_C18O_unc_constantTex_{0}K_mom0_pbcor_kink.fits'
 NC18Oplotname = 'N_C18O_constantTex_{0}K_mom0_pbcor.pdf'
 # constants
 rms = 0.40993961429269554 * u.K * u.km/u.s
-# The rms of the cube between 5.5 and 9.5 km/s is 0.41823096924675096 K
 X_C18O = 5.9e6  # Frerking et al 1982
 # this is the X_C18O value used in Nishimura et al 2015 for Orion clouds
 distance = (dist_Per50 * u.pc).to(u.cm)
 mu_H2 = 2.7
 B0 = (54891.420 * u.MHz).to(1/u.s)
-# You can use an array of temps or a ufloat
-Texlist = np.array([10, 11, 12, 13, 14, 15]) * u.K
+# In the future this can be changed to a T_ex map
 Tex_u = ufloat(15., 5.)
-# Tex_u = 15.
 '''
 End inputs
 '''
@@ -127,22 +88,41 @@ NC18Oheader['bunit'] = 'cm-2'
 formatname = str(int(Tex_u.n)) + 'pm' + str(int(Tex_u.s))
 # formatname = str(int(Tex_u))
 
-NC18O = N_C18O_21(mom0, B0, Tex_u)
-
-if not os.path.exists('column_dens_maps/'+NC18Ofilename.format(formatname)):
+if os.path.exists('column_dens_maps/'+NC18Ofilename.format(formatname)):
+    NC18O = fits.getdata('column_dens_maps/'+NC18Ofilename.format(formatname))
+    uNC18O = fits.getdata('column_dens_maps/'+uNC18Ofilename.format(formatname))
+    NC18O = unumpy.uarray(NC18O, uNC18O)
+else:
+    NC18O = N_C18O_21(mom0, B0, Tex_u)
     newfitshdu = fits.PrimaryHDU(data=unumpy.nominal_values(NC18O), header=NC18Oheader)
     newfitshdu.writeto('column_dens_maps/'+NC18Ofilename.format(formatname))
-if not os.path.exists('column_dens_maps/'+uNC18Ofilename.format(formatname)):
     newfitshdu = fits.PrimaryHDU(data=unumpy.std_devs(NC18O), header=NC18Oheader)
     newfitshdu.writeto('column_dens_maps/'+uNC18Ofilename.format(formatname))
 
-NC18Okink = N_C18O_21(mom0kink, B0, Tex_u)
-if not os.path.exists('column_dens_maps/'+NC18Ofilenamekink.format(formatname)):
+# if not os.path.exists('column_dens_maps/'+NC18Ofilename.format(formatname)):
+#     newfitshdu = fits.PrimaryHDU(data=unumpy.nominal_values(NC18O), header=NC18Oheader)
+#     newfitshdu.writeto('column_dens_maps/'+NC18Ofilename.format(formatname))
+# if not os.path.exists('column_dens_maps/'+uNC18Ofilename.format(formatname)):
+#     newfitshdu = fits.PrimaryHDU(data=unumpy.std_devs(NC18O), header=NC18Oheader)
+#     newfitshdu.writeto('column_dens_maps/'+uNC18Ofilename.format(formatname))
+
+if os.path.exists('column_dens_maps/'+NC18Ofilenamekink.format(formatname)):
+    NC18Okink = fits.getdata('column_dens_maps/'+NC18Ofilenamekink.format(formatname))
+    uNC18Okink = fits.getdata('column_dens_maps/'+uNC18Ofilenamekink.format(formatname))
+    NC18Okink = unumpy.uarray(NC18Okink, uNC18Okink)
+else:
+    NC18Okink = N_C18O_21(mom0kink, B0, Tex_u)
     newfitshdu = fits.PrimaryHDU(data=unumpy.nominal_values(NC18Okink), header=NC18Oheader)
     newfitshdu.writeto('column_dens_maps/'+NC18Ofilenamekink.format(formatname))
-if not os.path.exists('column_dens_maps/'+uNC18Ofilenamekink.format(formatname)):
     newfitshdu = fits.PrimaryHDU(data=unumpy.std_devs(NC18Okink), header=NC18Oheader)
     newfitshdu.writeto('column_dens_maps/'+uNC18Ofilenamekink.format(formatname))
+
+# if not os.path.exists('column_dens_maps/'+NC18Ofilenamekink.format(formatname)):
+#     newfitshdu = fits.PrimaryHDU(data=unumpy.nominal_values(NC18Okink), header=NC18Oheader)
+#     newfitshdu.writeto('column_dens_maps/'+NC18Ofilenamekink.format(formatname))
+# if not os.path.exists('column_dens_maps/'+uNC18Ofilenamekink.format(formatname)):
+#     newfitshdu = fits.PrimaryHDU(data=unumpy.std_devs(NC18Okink), header=NC18Oheader)
+#     newfitshdu.writeto('column_dens_maps/'+uNC18Ofilenamekink.format(formatname))
 
 # Now we take the total mass and estimate accretion
 if os.path.exists(tablefile_unumpy.format(formatname)):
@@ -163,6 +143,7 @@ else:
         MH2 = M_hydrogen2(NH2tot, mu_H2, distance, deltara, deltadec)
         results_mass_unumpy.loc[index, 'M (M_sun)'] = MH2.n
         results_mass_unumpy.loc[index, 'u M (M_sun)'] = MH2.s
+        results_mass_unumpy.loc[index, 't_freefall (yr)'] = t_ff.n
         dotM = MH2/t_ff
         results_mass_unumpy.loc[index, 'dotM_in (M_sun/yr) (M_star=0.39Msun)'] = dotM.n
         results_mass_unumpy.loc[index, 'u dotM_in (M_sun/yr)'] = dotM.s